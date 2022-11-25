af927b8c6425cafcef3b12a40b229581455c594e
Linken Dinh <linken.dinh@gmail.com>
2022-11-25 14:14:09 +0100

    bump dependencies

--- dist/esm/src/index.js
+++ dist/esm/src/index.js
@@ -1,4 +1,4 @@
-import * as baseESLint from "./eslint/base";
-import * as baseStylelint from "./stylelint/base";
+import * as baseESLint from '@this/src/eslint/base';
+import * as baseStylelint from '@this/src/stylelint/base';
 export const eslint = { base: baseESLint };
 export const stylelint = { base: baseStylelint };

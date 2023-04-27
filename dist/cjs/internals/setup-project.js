55d6df9611d8542c438ad5423a4cb2752e8e0b34
Andreas Savva <andreas.savva@unikum.net>
2023-04-27 17:09:04 +0300

    pnpm upgrade

--- dist/cjs/internals/setup-project.js
+++ dist/cjs/internals/setup-project.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
-const config_1 = tslib_1.__importDefault(require("../src/config"));
-const package_1 = require("../src/modules/package");
+const config_1 = tslib_1.__importDefault(require("@this/src/config"));
+const package_1 = require("@this/src/modules/package");
 class SetupProject {
     static setup = () => package_1.Package.syncPeerDependencies({
         pathFilePackageJSON: config_1.default.paths.files['package.json'],
--- dist/cjs/src/index.js
+++ dist/cjs/src/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.stylelint = exports.eslint = void 0;
 const tslib_1 = require("tslib");
-const baseESLint = tslib_1.__importStar(require("./eslint/base"));
-const baseStylelint = tslib_1.__importStar(require("./stylelint/base"));
+const baseESLint = tslib_1.__importStar(require("@this/src/eslint/base"));
+const baseStylelint = tslib_1.__importStar(require("@this/src/stylelint/base"));
 exports.eslint = { base: baseESLint };
 exports.stylelint = { base: baseStylelint };

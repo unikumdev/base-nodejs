cd9b9f204f5a3d39f80f8654bfce0a1af45f6d05
Maria Höglund <maria.hoglund@unikum.net>
2023-08-18 10:45:06 +0200

    revert prettierrc change

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,10 +1,10 @@
 module.exports = {
   arrowParens: 'always',
   bracketSpacing: true,
-  jsxBracketSameLine: false,
   printWidth: 80,
+  semi: false,
   singleQuote: true,
   tabWidth: 2,
   trailingComma: 'all',
   useTabs: false,
-}
+};

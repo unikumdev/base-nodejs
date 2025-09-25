083a4a8ae0f340fb7ff2271fb7d5782a591d28c9
Dagmawi Yehenew <dagmawi.yehenew@unikum.net>
2025-09-25 15:10:30 +0200

    chore: changesets automatically updated package versions

---
+++ version-and-publish-packages.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Versioning and publishing packages..."
+
+function write_npmrc () {
+  local PATH_FILE="${HOME}/.npmrc"
+  local LINES=(${NPMRC_COMA_SEPARATED//;/ })
+  touch "${PATH_FILE}"
+
+  for line in "${LINES[@]}"; do
+    echo "${line}" >>"${PATH_FILE}"
+  done
+}
+
+write_npmrc
+
+#pnpm changeset version || exit 1
+pnpm install || exit 1
+
+git add .
+git commit -m "chore: changesets automatically updated package versions"
+git push
+
+pnpm changeset publish || exit 1
